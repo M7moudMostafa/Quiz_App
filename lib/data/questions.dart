a14c9a6e88d887f01d310efd4f9d680638ed66f6
M7moudMostafa <mahmoud309mostafa@gmail.com>
2026-03-05 14:50:49 +0200

    Add Dummy Data

---
+++ lib/data/questions.dart
@@ -0,0 +1,22 @@
+import 'package:quiz_app/models/quiz_questions.dart';
+
+const questions = [
+  QuizQuestions("What's the capital of France?", [
+    "Berlin",
+    "Madrid",
+    "Paris",
+    "Rome",
+  ]),
+  QuizQuestions("What's the capital of Spain?", [
+    "Berlin",
+    "Madrid",
+    "Paris",
+    "Rome",
+  ]),
+  QuizQuestions("What's the capital of Italy?", [
+    "Berlin",
+    "Madrid",
+    "Paris",
+    "Rome",
+  ]),
+];
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,6 +12,19 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("Questions Screen"));
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("The question"),
+          const SizedBox(height: 30),
+          ElevatedButton(onPressed: () {}, child: const Text("Answer1")),
+          ElevatedButton(onPressed: () {}, child: const Text("Answer2")),
+          ElevatedButton(onPressed: () {}, child: const Text("Answer3")),
+          ElevatedButton(onPressed: () {}, child: const Text("Answer4")),
+        ],
+      ),
+    );
   }
 }
